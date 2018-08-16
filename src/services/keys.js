f9ee199ccd0e4266891418b386dfeec0ee563f21
tap4fun <tap4fun@T4F-M.local>
2018-08-16 10:29:55 +0800

    update keys

---
+++ src/services/keys.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var ak = exports.ak = 'YC8DWemOR8iZ12lUX7b63XAGWbNSgqmI';
+var amap_key = exports.amap_key = '1cc731d25deeffcf38849b28983b5d9c';
+var base_url = exports.base_url = 'http://172.20.120.180:3000/api/user';
---
+++ src/utils/keys.js
@@ -0,0 +1,4 @@
+export const ak = 'YC8DWemOR8iZ12lUX7b63XAGWbNSgqmI'
+export const amap_key = '1cc731d25deeffcf38849b28983b5d9c'
+export const base_url = 'http://172.20.120.180:3000/api'
+export const socket_url = 'http://172.20.120.180:3000/'
